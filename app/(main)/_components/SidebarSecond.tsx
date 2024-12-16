b0093433a80ffa9e60754d6b09a474b5044c855b
DanteTerry <sinasa88@gmail.com>
2024-12-16 23:19:57 +0100

    fix: New ui for sidebarSecond

--- app/(main)/_components/SidebarSecond.tsx
+++ app/(main)/_components/SidebarSecond.tsx
@@ -8,7 +8,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 // sidebar button details
 const SidebarButton = ({ icon: Icon, name }: { icon: any; name: string }) => (
-  <button className="flex w-full items-center gap-3 rounded-lg p-3 px-4 text-gray-500 transition-all duration-300 hover:bg-blue-500 hover:text-white">
+  <button className="flex w-full items-center gap-3 rounded-lg p-3 px-4 text-gray-500 transition-all duration-300 hover:bg-gradient-to-r hover:from-blue-500 hover:to-purple-500 hover:text-white">
     <Icon size={20} />
     <span>{name}</span>
   </button>
@@ -29,8 +29,8 @@ const SidebarLink = ({
   <Link
     href={href}
     className={cn(
-      "flex w-full items-center gap-3 rounded-lg p-3 px-4 text-gray-500 transition-all duration-300 hover:bg-blue-500 hover:text-white",
-      isActive && "bg-blue-500 text-white",
+      "flex w-full items-center gap-3 rounded-lg p-3 px-4 text-gray-500 transition-all duration-300 hover:bg-gradient-to-r hover:from-blue-500 hover:to-purple-500 hover:text-white",
+      isActive && "bg-gradient-to-r from-blue-500 to-purple-500 text-white",
     )}
   >
     <Icon size={20} />
@@ -43,7 +43,7 @@ function SidebarSecond() {
   const pathName = usePathname();
 
   return (
-    <aside className="fixed flex h-screen w-64 flex-col gap-5 bg-gradient-to-b from-gray-800 to-gray-700 p-6 shadow-2xl">
+    <aside className="fixed flex h-screen w-64 flex-col gap-5 bg-gradient-to-b from-gray-900 to-gray-800 p-6 shadow-2xl">
       <div className="flex items-center gap-3">
         <Image src="/logo/amplify.png" alt="logo" width={40} height={40} />
         <h3 className="text-2xl font-bold text-white">Amplify</h3>
@@ -73,7 +73,7 @@ function SidebarSecond() {
           />
         ))}
       </div>
-      <div className="mt-auto flex flex-col items-center gap-4 rounded-lg bg-blue-500 p-4 shadow-xl">
+      <div className="mt-auto flex flex-col items-center gap-4 rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 p-4 shadow-xl">
         <div className="flex items-center justify-center rounded-full border-4 border-gray-800 bg-white p-4 shadow-md">
           <FaLocationArrow size={20} className="text-blue-500" />
         </div>
